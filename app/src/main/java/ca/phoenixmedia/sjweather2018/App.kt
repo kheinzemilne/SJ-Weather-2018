6c6ad208b9ad2afc18ad69cde2dc9df89588f7b1
Kira <kira@phoenix-media.ca>
2023-03-31 15:37:17 -0300

    added a comment explaining App Class

--- app/src/main/java/ca/phoenixmedia/sjweather2018/App.kt
+++ app/src/main/java/ca/phoenixmedia/sjweather2018/App.kt
@@ -3,6 +3,9 @@ package ca.phoenixmedia.sjweather2018
 import android.app.Application
 import android.content.Context
 
+// I always have one of these in order to have a context available wherever it's needed.
+// This particular type of global context is not vulnerable to being leaked because it is tied
+// to the overall app lifecycle.
 class App : Application() {
     init {
         instance = this
